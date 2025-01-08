809d724c644f050c3c0be0220f91eaba2831af59
v3nicios <venicios707@gmail.com>
2025-01-08 17:53:06 -0400

    okay

---
+++ src/Produto.java
@@ -0,0 +1,30 @@
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private double valor;
+    private boolean entrega;
+
+    public Produto(int id, String nome, String descricao, double valor, boolean entrega ){
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.entrega = entrega;
+
+    }
+public int getID() {
+
+    return id;
+  }
+
+  
+
+
+
+
+
+
+
+
+}
